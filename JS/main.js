ca3753c868b3e9aea11a42fb41cae3389d8d7929
FRANCISCO SEVERINO <rickyventura271@gmail.com>
2022-04-17 00:54:57 -0400

    vehicle article

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+//guardar el estado (columna activa)
+
+let columnaActiva = 1;
+
+//Selecionar las columnas
+
+const columnas = document.querySelectorAll(".columna");
+
+//escuchar los "clicks" en casa una de ellas
+
+columnas.forEach((columna, indice) => {
+  columna.addEventListener("click", function () {
+    cambiarColumna(indice)
+  })
+})
+
+//cambiar el estado de las columnas
+
+function cambiarColumna(indice) {
+  columnas[columnaActiva].classList.remove("activa");
+  columnas[indice].classList.add("activa");
+  columnaActiva = indice;
+}
